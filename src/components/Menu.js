12ab44184c00f231baf7612b43cd7de93a31f976
noguewill <nogue.will@gmail.com>
2021-07-23 11:29:03 -0300

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -25,19 +25,22 @@ const Menu = ({ theme }) => {
   const [activeIndex, setActiveIndex] = useState(null);
 
   const renderedItems = menuObj.map((item, index) => {
-    const active = index === activeIndex ? `bg-white text-yellow-800` : "";
+    const active =
+      index === activeIndex
+        ? `"bg-${xpoThemes[theme].themeMainColor} ${xpoThemes[theme].themeMainColor}"`
+        : "";
     return (
       <React.Fragment key={index}>
         {/* <li className="pb-1 text-center text-dirtGold "> */}
         <Link
           to={item.href}
-          className={`2xl:pb-5 pb-1 text-center flex flex-col  text-yellow-800 menuItem 2xl:justify-around`}
+          className={`2xl:pb-5 pb-1 text-center flex flex-col  ${xpoThemes[theme].themeMainColor} menuItem 2xl:justify-around`}
         >
           <button
             id={index}
             href={item.href}
             onClick={() => onTitleClick(index)}
-            className={`${active}  2xl:text-2xl  text-yellow-800  text-center hover:underline pb-1 px-5 hover:text-yellow-400 transition ease font-light tracking-widestxl`}
+            className={`${active}  2xl:text-2xl   text-center hover:underline pb-1 px-5 hover:text-black transition ease font-light tracking-widestxxl"`}
           >
             {item.text}
           </button>
@@ -48,7 +51,7 @@ const Menu = ({ theme }) => {
 
   return (
     <>
-      <li className="pb-1 text-yellow-800">{renderedItems}</li>
+      <li className="pb-1">{renderedItems}</li>
     </>
   );
 };
